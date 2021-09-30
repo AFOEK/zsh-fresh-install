210b4da2dc720b233af3f6809c00beffe413304b
AFOEK <felix.montalfu@gmail.com>
2021-09-30 11:34:33 +0700

    Adding architecture (x86, x86_64, aarch64, and armv7l)

--- fresh_install_zsh.sh
+++ fresh_install_zsh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 platform='unknow'
 unamestr=$(uname)
+arch=$(uname -m)
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$NAME
@@ -26,13 +27,29 @@ else
     VER=$(uname -r)
 fi
 
-if [[ "$OS" == "Ubuntu" ] || [ "$OS" == "Raspbian GNU/Linux" ]]; then
-    sudo apt-get install -y zsh wget git lolcat figlet
+if [[ "$OS" == "Ubuntu" || "$OS" == "Debian" ]]; then
+    sudo apt-get install -y zsh wget git lolcat figlet exa
 elif [[ "$OS" == "Darwin" ]]; then
     if ! [ -x "$(command -v brew)" ]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
-    sudo brew install lolcat figlet
+    sudo brew install lolcat figlet exa
 elif [[ "$OS" == "ManjaroLinux" ]]; then
-    sudo pacman -Syu zsh
+    sudo pacman -Syu zsh lolcat figlet exa
+fi
+echo "Installing " && sleep 5
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [[ "$OS" == "Ubuntu" ]]; then
+    cp zshrc_ubuntu $HOME/.zshrc
+elif [[ "$OS" == "Darwin" ]]; then
+    cp zshrc_macos $HOME/.zshrc
+elif [[ "$OS" == "ManjaroLinux" ]]; then
+    cp zshrc_majaro $HOME/.zshrc
+elif [[ "$OS" -eq "Debian" ]] && ([[ "$arch" -eq "x86_64" ] && [ "$arch" -eq "x86" ]]); then
+    cp zshrc_debian $HOME/.zshrc
+elif [[ "$OS" -eq "Debian" ]] && ([ "$arch" -eq "armv7l" ] && [ "$arch" -eq "aarch64" ]); then
+    cp zshrc_raspberry $HOME/.zshrc
+else
+    echo "Not recognize operating system or architecture"
 fi
